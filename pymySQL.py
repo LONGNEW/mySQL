67fa4e7fc6bd150a18abf671d4198c96bf46b098
ShinHyoung <jsin2475@naver.com>
2022-02-01 21:04:56 +0900

    modify : foreign key 2
    - 참조 당하고 있는 테이블 : 참조 하는 테이블에 해당 키를 가진 데이터가 있으면 삭제 시 에러.
    - 참조 하는 테이블 : 당하는 테이블에 해당 키를 가진 데이터가 없으면 에러
    - 이를 통해 데이터를 저장하기 위한, 삭제하기 위한 조건을 알 수 있음.

--- pymySQL.py
+++ pymySQL.py
@@ -9,11 +9,9 @@ f.close()
 db = pymysql.connect(host=host, port=port, user=user, passwd=passwd, db=db, charset=charset)
 cursor = db.cursor()
 
-# 현재 TABLE "bytTbl"에서 userID가 "userTbl"의 아이디를 참조하는 foriegn key로 설정.
-# 그럴 경우 추가 되는 데이터의 userID가 "userTbl"에 존재하지 않는 경우 에러가 발생하게 됨.
 sql = \
 f"""
-    INSERT INTO buyTbl (userID, prodName, groupName, price, amount) VALUES('STJ', '운동화', '의류', 30, 2);
+    delete from usertbl where userid="STJ ";
 """
 cursor.execute(sql)
 
