aa3a6a80fef4c34812b8a424d1e34be0a8164004
ShinHyoung <jsin2475@naver.com>
2022-02-01 16:09:26 +0900

    modify : creating table
    - 커서를 통해 명령어를 입력
    - DB 커넥션을 통해 쿼리 실행

--- pymySQL.py
+++ pymySQL.py
@@ -11,3 +11,19 @@ db = pymysql.connect(host=host, port=port, user=user, passwd=passwd, db=db, char
 # 입력하는 실해창을 가져오는 방식.
 # 커서를 통해 입력을 하게 됨.
 cursor = db.cursor()
+sql = """
+        create table product(
+            product_code varchar(20) not null,
+            title varchar(200) not null,
+            ori_price int,
+            discount_price int,
+            discount_percent int,
+            delivery varchar(2),
+            primary key(product_code)
+        );
+        """
+cursor.execute(sql)
+
+# 모든 쿼리를 실행하도록 하는 함수 (데이터를 변환하고, 복원이 불가한 상황으로 가게 됨)
+db.commit()
+db.close()
