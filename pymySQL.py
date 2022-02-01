5b579186bde91a4134c35fe97c95bc47aac3e8b4
ShinHyoung <jsin2475@naver.com>
2022-02-01 16:31:13 +0900

    modify : SELECT의 결과 출력하기.
    - fetchall()
    - fetchmany(size)
    - fetchone()
    - 함수를 통해 출력값 개수만큼 가져올 수 있음.

--- pymySQL.py
+++ pymySQL.py
@@ -8,19 +8,16 @@ f.close()
 db = pymysql.connect(host=host, port=port, user=user, passwd=passwd, db=db, charset=charset)
 cursor = db.cursor()
 
-# f"" 을 통해 더 편하게 sql 구문을 만들기.
-for idx in range(10):
-    code = 215673140 + idx
-    sql = f"""insert into product 
-    values ({code}, "스위트 바니 여름신상", 23000, 6900, 70, "F");
-    """
-    cursor.execute(sql)
+sql = f"""
+    select * from product;
+"""
+cursor.execute(sql)
 
-# # fetchall()을 통해 출력된 모든 결과들을 가져옴.
-# ret = cursor.fetchall()
-#
-# for item in ret:
-#     print(*item)
+# fetchall()을 통해 출력된 모든 결과들을 가져옴.
+ret = cursor.fetchall()
+
+for item in ret:
+    print(*item)
 
 db.commit()
 db.close()
