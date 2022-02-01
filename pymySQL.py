f76b3cac10bf6b3d275380459590e71a961faf70
ShinHyoung <jsin2475@naver.com>
2022-02-01 17:00:34 +0900

    modify : UPDATE 하는 방식
    - UPDATE 구문 사용.

--- pymySQL.py
+++ pymySQL.py
@@ -9,15 +9,20 @@ db = pymysql.connect(host=host, port=port, user=user, passwd=passwd, db=db, char
 cursor = db.cursor()
 
 sql = f"""
-    select * from product;
+    update product set 
+    title="원피스 가디건 코디전", 
+    ori_price=33000, 
+    discount_price=9900, 
+    discount_percent=70
+    where product_code="215673141"
 """
 cursor.execute(sql)
 
-# fetchall()을 통해 출력된 모든 결과들을 가져옴.
-ret = cursor.fetchall()
-
-for item in ret:
-    print(*item)
+# # fetchall()을 통해 출력된 모든 결과들을 가져옴.
+# ret = cursor.fetchall()
+#
+# for item in ret:
+#     print(*item)
 
 db.commit()
 db.close()
