4ccf68f40f5bd7b8a3fe6b4084f371e313c3b8a9
ShinHyoung <jsin2475@naver.com>
2022-02-02 15:32:15 +0900

    modify : 추가적인 함수, subcategory 가져오는 방식
    - 에러가 생길 수 있는 경우 확인
    - 원하는 정보만 골라서 파싱.

--- crawlToSQL.py
+++ crawlToSQL.py
@@ -24,11 +24,39 @@ def close_db(db):
     db.commit()
     db.close()
 
+def get_provider():
+    ret =
+    return ret
+
+def get_items(html, name, sub_name):
+    ret = []
+    items = html.select("div.best-list")
+
+    # HTML에서 동일한 클래스를 가진 다른 2개가 존재함.
+    # 원하는 정보를 골라서 사용하는 방식.
+    # 중요! get_text()와 같은 함수는 결국 그 객체가 존재해야 함.
+    # 존재하지 않으면 당연히 에러가 발생하게 됨.
+    for idx, item in enumerate(items[1].select("li")):
+        title = item.select_one("a.itemname").get_text()
+        ori_price = item.select_one("div.o-price").get_text()
+
+        # 여러 태그들을 확인한 후에 모든 태그에 맞는 코드 생성.
+        dis_price = item.select_one("div.s-price strong span").get_text()
+        dis_percent = item.select_one("div.s-price em").get_text()
+
+
+def sub(link, name):
+    res = requests.get(link)
+    soup = BeautifulSoup(res.content, "html.parser")
+
+    categories = soup.select("div.navi.group a")
+    for item in categories:
+        print(item.get_text())
 
 res = requests.get("http://corners.gmarket.co.kr/Bestsellers")
 soup = BeautifulSoup(res.content, "html.parser")
 
 categories = soup.select("#categoryTabG li a")
-# bs4에서 링크를 가져오는 법, 내부 텍스트를 가져오는 방법.
 for item in categories:
-    print(item["href"], item.get_text())
+    sub("http://corners.gmarket.co.kr" + item["href"], item.get_text())
+
